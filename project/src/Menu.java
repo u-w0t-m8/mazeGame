ab9d658c5dc156b625f21ad25012db95d22597e7
Jes040 <jessicatsang32@gmail.com>
2016-05-08 17:39:03 +1000

    Menu implementation and comments

--- project/src/Menu.java
+++ project/src/Menu.java
@@ -1,105 +1,42 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.util.List;
 
-import javax.imageio.ImageIO;
-import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.Border;
-
-/**
- * Listens for
- *
- */
-public class Menu {
-
-	//private BufferedImage titleImage = ImageIO.read(new File("/imgs/wobcke.jpg"));
+public class Menu{
 	
-	private List<String> items;
-	private int selected = 0;
-	private int width = 800; 	// hard coded width & height needs to be replaced by 
-	private int height = 600; 	// something which passes in the resolution size
-	private JFrame menuFrame;
-
-	private Border border;
-	
-	
+	private int selected;
+			
 	/**
 	 * Constructor for menu
-	 * NOTE: things we might need for a menu
-	 * we may need to pass in some sort of input so we can "click the start button"
-	 * may need to grab arguments from input class
+	 * set the selected difficulty to whatever input has passed in.
 	 */
 	public Menu() {	
-		
-		initialiseMenuFrame();
-
+		selected = 0;
 	}
 	
 	/**
-	 * Renders the menu frame
-	 * 
-	 * 
+	 * Get the value of selected 
+	 * 0 = easy
+	 * 1 = medium
+	 * 2 = hard
+	 * @return int
 	 */
-	public void initialiseMenuFrame() {
-
-		//Creates a new window 800 x 600]
-		menuFrame = new JFrame("4W3$oM3 Maze Game YEAAA pls buy DLC");
-        menuFrame.setSize(800, 600);
-        menuFrame.setLocationRelativeTo(null);
-		menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JPanel title = new JPanel();
-		title.setLayout(new BorderLayout());
-		title.setBorder(border);
-		title.setPreferredSize(new Dimension(width, height/2));
-		title.setBorder(BorderFactory.createLineBorder(Color.red));
-
-		
-		menuFrame.add(title);
-		menuFrame.setVisible(true);
+	private int getSelected() {
+		return selected;
 	}
-	
-	
+
 	/**
-	 * Select specific button
+	 * Change the selected value if key pressed is up
 	 */
-	public void selector(){
-		
-		
-	}
-	
-	public void addItems(String... text){
-		for (String s: text){
-			items.add(s);
-		}
-	}
-	
-	public String getSelectedItem(){
-		return items.get(selected);
-	}
-	
-	public String getItem(int index){
-		return items.get(index);
-	}
-	
 	public void up(){
-		selected--;
-		if (selected < 0){
-			selected = items.size()-1;
+		if ( (selected >= 0)  && (selected<= 2)){
+			selected++;
 		}
 	}
 	
+	/**
+	 * Change the selected value if key pressed is down
+	 */
 	public void down(){
-		if (selected >= items.size()){
-			selected = 0;
+		if ( (selected >= 0)  && (selected<= 2)){
+			selected--;
 		}
 	}
-
 }
