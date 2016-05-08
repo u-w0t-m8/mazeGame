0074cf30b3657ee6737847a5306233108f7a9db9
Eric Sy <Eric Sy@192.168.11.14>
2016-05-08 13:00:54 +1000

    update

--- project/src/Input.java
+++ project/src/Input.java
@@ -1,4 +1,43 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+/**
+ * Listens and interprets the input made by the user
+ * from both the main menu and the game itself
+ * 
+ */
+public class Input implements KeyListener {
 
-public class Input {
+	public Input(){
+		
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int keyCode = e.getKeyCode();
 
+		//System.exit(0);			
+		//if( (keyCode == KeyEvent.VK_ESCAPE) || (keyCode == KeyEvent.VK_Q) )
+		// If user pressed any of the keys program terminates
+		if(keyCode == KeyEvent.VK_SPACE)
+		{
+				
+		}
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+    
 }
