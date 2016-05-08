7745a580390890adeb12067748906530b9aca236
Jasonvooo <jasonvo1997@gmail.com>
2016-05-08 18:03:25 +1000

    drawMenu

--- project/src/Renderer.java
+++ project/src/Renderer.java
@@ -72,9 +72,24 @@ public class Renderer {
         }
     }
 
+    /**
+     * Setting up Graphics
+     */
     public void drawMenu(Menu m){
-        //TODO
+    	frameGraphics.drawString("Maze Game", m.getWidth()/2, m.getHeight()/4);
+    	frameGraphics.drawRect(0, 0, m.getWidth(), m.getHeight()/2);
+ 
+    	
+    	frameGraphics.drawString("EASY", m.getWidth()/2, m.getHeight()5/8);
+    	frameGraphics.drawRect(0, m.getHeight()/2, m.getWidth(), m.getHeight()/8);
+    	
+    	frameGraphics.drawString("MEDIUM", m.getWidth()/2, m.getHeight()6/8);
+    	frameGraphics.drawRect(0, m.getHeight()5/8, m.getWidth(), m.getHeight()/8);
+    	
+    	frameGraphics.drawString("HARD", m.getWidth()/2, m.getHeight()7/8);
+    	frameGraphics.drawRect(0, m.getHeight()6/8, m.getWidth(), m.getHeight()/8);
     }
+
     
     public void createPreRender(Grid grid) {
         // make mazeBackground a new image
