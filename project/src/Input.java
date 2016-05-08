9fee13bf2fe351585d8a1247443e116cc9ed0693
Eric Sy <Eric Sy@192.168.11.14>
2016-05-08 13:16:07 +1000

    updated input

--- project/src/Input.java
+++ project/src/Input.java
@@ -7,22 +7,33 @@ import java.awt.event.KeyListener;
  */
 public class Input implements KeyListener {
 
-	public Input(){
-		
+	private Engine engine;
+	
+	public Input(Engine engine){
+		this.engine = engine;
 	}
 	
 	@Override
 	public void keyPressed(KeyEvent e) {
 		int keyCode = e.getKeyCode();
-
-		//System.exit(0);			
-		//if( (keyCode == KeyEvent.VK_ESCAPE) || (keyCode == KeyEvent.VK_Q) )
-		// If user pressed any of the keys program terminates
-		if(keyCode == KeyEvent.VK_SPACE)
-		{
-				
+		if(keyCode == KeyEvent.VK_UP){
+			engine.pressUp();
 		}
-		// TODO Auto-generated method stub
+		else if(keyCode == KeyEvent.VK_DOWN){
+			engine.pressDown();
+		}
+		else if(keyCode == KeyEvent.VK_LEFT){
+			engine.pressLeft();
+		}
+		else if(keyCode == KeyEvent.VK_RIGHT){
+			engine.pressRight();
+		}
+		else if(keyCode == KeyEvent.VK_ENTER){
+			engine.pressEnter();
+		}
+		else if(keyCode == KeyEvent.VK_ESCAPE){
+			System.exit(0);
+		}
 		
 	}
 
