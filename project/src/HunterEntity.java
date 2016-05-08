9d6c7e4fc68b4ab67810b6a788cbc9a4fa083157
Eric Sy <Eric Sy@ERIC-XPS13>
2016-05-08 20:00:53 +1000

    1o gogogo

--- project/src/HunterEntity.java
+++ project/src/HunterEntity.java
@@ -14,6 +14,7 @@ public class HunterEntity extends LivingEntity {
 		// TODO Auto-generated constructor stub
 	}
 	
+	
 	/**
 	 * Performs A* search with straight line distance squared
 	 * @param grid - grid of the map
